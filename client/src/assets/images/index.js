2a769560b54e62ed122888f3fdad83c133cbd4f0
Abhijit <paulabhijit2607@gmail.com>
2025-02-27 15:00:42 +0530

    Added assets.

---
+++ client/src/assets/images/index.js
@@ -0,0 +1,4 @@
+export { default as Logo } from './Logo.png';
+export { default as logoTransparent } from './logoTransparent.png';
+export { default as logoTransparentLarge } from './logoTransparentLarge.png';
+export { default as stars } from './stars.svg';
