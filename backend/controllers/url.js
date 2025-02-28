38da74de3b454eb37a71c9dca93e6c5d3c7cc2ee
Abhijit <paulabhijit2607@gmail.com>
2025-02-28 11:22:37 +0530

    Added backend form validation
    Checks if the URL starts with 'https://' or not, and sends response
    accordingly.

--- backend/controllers/url.js
+++ backend/controllers/url.js
@@ -4,6 +4,7 @@ const URL = require('../models/url');
 async function handleGenerateShortURL(req, res) {
     const body = req.body;
     if (!body.url) return res.status(400).json({ error: 'URL is requried.' });
+    if (!body.url.startsWith('https://')) return res.status(400).json({error: 'URL must start with "https://"'})
     const shortID = nanoid(8);
     await URL.create({
         user: body.user,
